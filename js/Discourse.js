0463c85f382d6249540b3f0d84278cfe52550d2b
Cory Reid <cory@coryjreid.com>
2020-05-16 15:47:25 -0600

    Get the code compiling with all the latest tools and integrate OneSignal test code

--- js/Discourse.js
+++ js/Discourse.js
@@ -33,6 +33,7 @@ import type {Notification, NotificationOpen} from './firebase/helper';
 import bgMessaging from './firebase/bgMessaging';
 import BackgroundFetch from 'react-native-background-fetch';
 import AsyncStorage from '@react-native-community/async-storage';
+import OneSignal from 'react-native-onesignal';
 
 const AppNavigator = createStackNavigator(
   {
@@ -91,6 +92,20 @@ class Discourse extends React.Component {
           this._handleRemoteNotification(e);
         }
       });
+
+      //Remove this method to stop OneSignal Debugging 
+      OneSignal.setLogLevel(6, 0);
+
+      // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+      OneSignal.init("673809b0-5765-45e1-bd94-3d02b8a3a5c8", {kOSSettingsKeyAutoPrompt : false, kOSSettingsKeyInAppLaunchURL: false, kOSSettingsKeyInFocusDisplayOption:2});
+      OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+
+      // The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+      OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+      OneSignal.addEventListener('received', this.onReceived);
+      OneSignal.addEventListener('opened', this.onOpened);
+      OneSignal.addEventListener('ids', this.onIds);
     }
 
     if (Platform.OS === 'android') {
@@ -158,6 +173,27 @@ class Discourse extends React.Component {
         },
       );
     }
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
   }
 
   // _handleLocalNotification(e) {
@@ -408,6 +444,10 @@ class Discourse extends React.Component {
       </ThemeContext.Provider>
     );
   }
+}
+
+function myiOSPromptCallback(permission){
+  // do something with permission value
 }
 
 export default Discourse;
